636a2f4d88386f1c1ee040ed5a19838600209360
ProkopenkoIA <boroda5384@mail.ru>
2025-06-01 01:32:08 +0500

    Коммит 7

---
+++ Lsn6HomeWork/src/test/java/com/example/MontyHallParadox/MontyHallParadoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MontyHallParadox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MontyHallParadoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
